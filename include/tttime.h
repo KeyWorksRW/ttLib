42f08ab07b4c08b17f6ce5df0ac4a2ef56abc33a
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2019-01-17 07:27:46 -0800

    Changes class name to ttTime and overhauls functionality

--- include/tttime.h
+++ include/tttime.h
@@ -1,9 +1,9 @@
 /////////////////////////////////////////////////////////////////////////////
-// Name:		CKeyTime
-// Purpose:		Class for handling Windows time structure
-// Author:		Ralph Walden (randalphwa)
-// Copyright:   Copyright (c) 2002-2018 KeyWorks Software (Ralph Walden)
-// License:     Apache License (see ../LICENSE)
+// Name:		ttTime
+// Purpose:		Class for handling a Windows SYSTEMTIME or FILETIME structure
+// Author:		Ralph Walden
+// Copyright:	Copyright (c) 2002-2019 KeyWorks Software (Ralph Walden)
+// License:		Apache License (see ../LICENSE)
 /////////////////////////////////////////////////////////////////////////////
 
 #pragma once
@@ -12,69 +12,80 @@
 #define __TTLIB_KTIME_H__
 
 #ifndef _WINDOWS_
-	#error CKeyTime can only be used when building for Windows. Use wxWidgets file-io for cross-platform functionality
+	#error ttTime can only be used when building for Windows. Use wxWidgets file-io for cross-platform functionality
 #endif
 
-#include "cstr.h"	// CStr
+#include "ttstring.h"	// ttString
 
 // This is a header-only class.
 
-class CKeyTime
+class ttTime
 {
 public:
-	CKeyTime(FILETIME* pftm) { ASSERT(pftm); ConvertFileTime(pftm); }
-	CKeyTime() { GetLocalTime(); }
-	~CKeyTime() {  }
+	ttTime() { GetLocalTime(); }
+	ttTime(FILETIME* pftm, bool bLocalTime = false) { ttASSERT(pftm); ConvertFileTime(pftm, bLocalTime); }	// Copies the file time into a SYSTEMTIME structure
+	~ttTime() { }
 
 	int GetYear() const { return (int) m_tm.wYear; }
-	int GetMonth() const { return (int) m_tm.wMonth; }      // month of year (1 = Jan)
-	int GetDay() const { return (int) m_tm.wDay; }         // day of month
+	int GetMonth() const { return (int) m_tm.wMonth; }	   // month of year (1 = Jan)
+	int GetDay() const { return (int) m_tm.wDay; }		   // day of month
 	int GetHour() const { return (int) m_tm.wHour; }
 	int GetMinute() const { return (int) m_tm.wMinute; }
 	int GetSecond() const { return (int) m_tm.wSecond; }
 	int GetMilliSecond() const { return (int) m_tm.wMilliseconds; }
-	int GetDayOfWeek() const { return (int) m_tm.wDayOfWeek; }  // 1=Sun, 2=Mon, ..., 7=Sat
+	int GetDayOfWeek() const { return (int) m_tm.wDayOfWeek; }	// 1=Sun, 2=Mon, ..., 7=Sat
 
 	const char* GetDateFormat(DWORD dwFlags = 0, LCID locale = LOCALE_USER_DEFAULT) {
-				char szBuf[256];
-				::GetDateFormat(locale, dwFlags, &m_tm, NULL, szBuf, sizeof(szBuf));
-				m_cszFormatted = szBuf;
-				return m_cszFormatted;
-	}
+					char szBuf[256];
+					::GetDateFormat(locale, dwFlags, &m_tm, NULL, szBuf, sizeof(szBuf));
+					m_cszDate = szBuf;
+					return m_cszDate;
+				}
 	const char* GetTimeFormat(DWORD dwFlags = 0, LCID locale = LOCALE_USER_DEFAULT) {
-				char szBuf[256];
-				::GetTimeFormat(locale, dwFlags, &m_tm, NULL, szBuf, sizeof(szBuf));
-				m_cszFormatted = szBuf;
-				return m_cszFormatted;
-	}
+					char szBuf[256];
+					::GetTimeFormat(locale, dwFlags, &m_tm, NULL, szBuf, sizeof(szBuf));
+					m_cszTime = szBuf;
+					return m_cszTime;
+				}
 	const char* GetFullFormat() {	// full date/time
-			char szBuf[256];
-			tt::strcpy(szBuf, sizeof(szBuf), GetDateFormat(NULL, DATE_LONGDATE));
-			tt::strcat(szBuf, ", ");
-			tt::strcat(szBuf, sizeof(szBuf) + tt::strlen(szBuf), GetTimeFormat());
-			m_cszFormatted = szBuf;
-			return m_cszFormatted;
-	}
+					m_cszFull = GetDateFormat(DATE_LONGDATE); // GetDateFormat() modifies m_cszFormatted, so we have to use a temporary
+					m_cszFull += ", ";
+					m_cszFull += GetTimeFormat();
+					return m_cszFull;
+				}
 	const char* GetShortFormat() {	// short date/time
-			char szBuf[256];
-			tt::strcpy(szBuf, sizeof(szBuf), GetDateFormat());
-			tt::strcat(szBuf, ", ");
-			tt::strcat(szBuf, sizeof(szBuf) + tt::strlen(szBuf), GetTimeFormat());
-			m_cszFormatted = szBuf;
-			return m_cszFormatted;
-	}
+					m_cszShort = GetDateFormat();	// GetDateFormat() modifies m_cszFormatted, so we have to use a temporary
+					m_cszShort += ", ";
+					m_cszShort += GetTimeFormat();
+					return m_cszShort;
+				}
 
 	void GetLocalTime() { ::GetLocalTime(&m_tm); }
 	void GetSystemTime() { ::GetSystemTime(&m_tm); }
 
-	void ConvertFileTime(const FILETIME* pftm) { ASSERT(pftm); FileTimeToSystemTime(pftm, &m_tm); }
-	void SysTimeToFileTime(FILETIME* pftm) { ASSERT(pftm); SystemTimeToFileTime(&m_tm, pftm); }
+	void ConvertFileTime(const FILETIME* pftm, bool bLocalTime = false) {
+				ttASSERT(pftm);
+				if (bLocalTime)	{
+					FILETIME tmLocal;
+					FileTimeToLocalFileTime(pftm, &tmLocal);
+					FileTimeToSystemTime(&tmLocal, &m_tm);
+				}
+				else
+					FileTimeToSystemTime(pftm, &m_tm);
+			}
+	void SysTimeToFileTime(FILETIME* pftm) { ttASSERT(pftm); SystemTimeToFileTime(&m_tm, pftm); }
 
 	operator SYSTEMTIME*() { return &m_tm; }
 
 protected:
 	SYSTEMTIME m_tm;
-	CStr m_cszFormatted;
+
+	// We keep a copy of each format in case we're called multiple times in a printf() call
+
+	ttString m_cszDate;
+	ttString m_cszTime;
+	ttString m_cszFull;
+	ttString m_cszShort;
 };
 
 #endif	// __TTLIB_KTIME_H__
