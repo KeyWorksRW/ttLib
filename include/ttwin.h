0b56b1b3b9f3a7a49b3768f7814a9a1eac290a0a
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2019-01-25 09:39:38 -0800

    Initial commit of ttWin class

---
+++ include/ttwin.h
@@ -0,0 +1,73 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:		ttWin
+// Purpose:		Class for working with windows
+// Author:		Ralph Walden
+// Copyright:	Copyright (c) 2018-2019 KeyWorks Software (Ralph Walden)
+// License:		Apache License (see LICENSE)
+/////////////////////////////////////////////////////////////////////////////
+
+// Do NOT use this class to create a MDI windows!
+
+#pragma once
+
+#ifndef _WINDOWS_
+	#error This code will only work on Windows
+#endif
+
+#include "../ttLib/include/ttmsgmap.h"	// #define macros for BEGIN_TTMSG_MAP()/END_TTMSG_MAP() block
+
+namespace ttpriv {
+	LRESULT WINAPI ttWinProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+}
+
+class ttWin		// Non-MDI window class
+{
+public:
+	ttWin();
+	~ttWin();
+
+	virtual bool OnMsgMap(UINT /* uMsg */, WPARAM /* wParam */, LPARAM /* lParam */) { return false; }	  // Use of BEGIN_TTMSG_MAP will override this
+
+	// Call these methods before calling CreateWnd
+
+	void SetClassBkgrnd(HBRUSH hbkgrnd) { if (m_pwc) m_pwc->hbrBackground = hbkgrnd; }	// constructor will have set this to COLOR_WINDOW + 1
+	void SetClassCursor(HCURSOR hcur) { if (m_pwc) m_pwc->hCursor = hcur; }
+	void SetClassIcon(size_t idIcon) { if (m_pwc) m_pwc->hIcon = LoadIcon(m_pwc->hInstance, (LPCTSTR) idIcon); }
+	void SetClassMenu(size_t idMenuResource) { if (m_pwc) m_pwc->lpszMenuName = (LPCTSTR) idMenuResource; }
+	bool SetClassName(const char* pszClassName);	// returns false if strlen(pszClassName) > 255
+	void SetClassSmallIcon(size_t idIcon) { if (m_pwc) m_pwc->hIconSm = LoadIcon(m_pwc->hInstance, (LPCTSTR) idIcon); }
+	void SetClassStyle(DWORD style) { if (m_pwc) m_pwc->style = style; }	// constructor will have set this to CS_HREDRAW | CS_VREDRAW
+
+	bool CreateWnd(const char* pszTitle, DWORD dwExStyle, DWORD dwStyle, HWND hwndParent = NULL, RECT* prc = NULL, HMENU hmenu = NULL);
+
+	bool AttachWnd(HWND hwnd);			// attaches to a window not created by ttWin, updates m_pszClassName, m_hwnd and m_hwndParent
+	bool SubClass(HWND hwnd = NULL);	// if NULL, subclass our own window
+
+	// Class functions
+
+	LRESULT SendMessage(UINT msg, WPARAM wParam = 0, LPARAM lParam = 0) const { return ::SendMessage(*this, msg, wParam, lParam); }
+	LRESULT PostMessage(UINT msg, WPARAM wParam = 0, LPARAM lParam = 0) const { return ::PostMessage(*this, msg, wParam, lParam); }
+
+	void ShowWindow(int nCmdShow = SW_SHOW) { ::ShowWindow(*this, nCmdShow); }
+
+	// For other Windows functions requiring an HWND parameter, simply pass in *this as the HWND
+
+	////////// Operators //////////
+
+	operator HWND() const { return m_hwnd; }
+	operator WNDCLASSEX*() { return m_pwc; }
+
+protected:
+	friend LRESULT WINAPI ttpriv::ttWinProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+	// Class members
+
+	HWND		m_hwnd;
+	HWND		m_hwndParent;
+	HINSTANCE	m_hinst;			// instance used to create the class, can be used to load resources from the app
+	const char*	m_pszClassName;		// class name of the window we created or attached to
+
+	WNDCLASSEXA* m_pwc; 			// created in constructor, deleted by CreateWnd and AttachWnd
+	WNDPROC		 m_SubClassProc;	// previous window procedure before it was subclassed
+	LRESULT		 m_result;
+};
---
+++ src/ttwin.cpp
@@ -0,0 +1,154 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:		ttWin
+// Purpose:		Class for working with windows
+// Author:		Ralph Walden
+// Copyright:	Copyright (c) 2018-2019 KeyWorks Software (Ralph Walden)
+// License:		Apache License (see LICENSE)
+/////////////////////////////////////////////////////////////////////////////
+
+#include "pch.h"
+
+#include "ttwin.h"	// ttWin
+
+// This is the Window procedure used by all windows that ttWin created or subclassed.
+
+LRESULT WINAPI ttpriv::ttWinProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	if (msg == WM_CREATE) {
+		CREATESTRUCT* pcs = (CREATESTRUCT*) lParam;
+		void* pThis = (void*) pcs->lpCreateParams;
+		if (pThis)
+			SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR) pThis);
+	}
+
+	ttWin* pThis = (ttWin*) GetWindowLongPtr(hwnd, GWLP_USERDATA);
+	if (!pThis)
+		return DefWindowProc(hwnd, msg, wParam, lParam);
+
+	if (pThis->OnMsgMap(msg, wParam, lParam))
+		return pThis->m_result;
+
+	if (pThis->m_SubClassProc)
+		return CallWindowProc(pThis->m_SubClassProc, hwnd, msg, wParam, lParam);
+
+	switch (msg) {
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			break;
+
+		default:
+			return DefWindowProc(hwnd, msg, wParam, lParam);
+	}
+
+	return 0;
+}
+
+ttWin::ttWin()
+{
+	m_hwnd = NULL;
+	m_hwndParent = NULL;
+	m_SubClassProc = nullptr;
+
+	// We create this here so the caller can modify it before calling CreateWnd
+
+	m_pwc = new WNDCLASSEX;
+	memset(m_pwc, 0, sizeof(WNDCLASSEXA));
+	m_pwc->cbSize = sizeof(WNDCLASSEXA);
+	m_pwc->lpfnWndProc = ttpriv::ttWinProc;
+	m_pwc->style = CS_HREDRAW | CS_VREDRAW;
+	m_pwc->hbrBackground = (HBRUSH) (LONG_PTR) (COLOR_WINDOW + 1);
+}
+
+ttWin::~ttWin()
+{
+	if (m_pwc)	// this will have already been deleted if CreateWnd or Attach was called
+		delete m_pwc;
+	if (m_pszClassName)
+		::free((void*) m_pszClassName);
+}
+
+bool ttWin::SetClassName(const char* pszClassName)
+{
+	ttASSERT_NONEMPTY(pszClassName);
+
+	if (!pszClassName || !*pszClassName || tt::strlen(pszClassName) > 256)	// Windows limits class names to 256 characters
+		return false;
+
+	if (m_pszClassName)
+		::free((void*) m_pszClassName);
+	m_pszClassName = _strdup(pszClassName);
+	if (m_pwc)
+		m_pwc->lpszClassName = m_pszClassName;
+	return true;
+}
+
+bool ttWin::CreateWnd(const char* pszTitle, DWORD dwExStyle, DWORD dwStyle, HWND hwndParent, RECT* prcPosition, HMENU hmenu)
+{
+	if (m_pwc) {	// means the class hasn't been registered yet
+		if (!m_pszClassName) {
+			ttStr cszClass(32);
+			cszClass.strcpy("ttWin");
+			tt::hextoa(GetTickCount(), (char*) cszClass + cszClass.strlen(), true);
+			m_pszClassName = _strdup(cszClass);
+			m_pwc->lpszClassName = m_pszClassName;
+		}
+		m_hinst = m_pwc->hInstance = GetModuleHandle(NULL);
+		if (::RegisterClassExA(m_pwc) == 0)
+			return false;
+
+		delete m_pwc;
+		m_pwc = nullptr;
+	}
+
+	m_hwndParent = hwndParent;
+
+	if (prcPosition != NULL)
+		m_hwnd = ::CreateWindowExA(dwExStyle, m_pszClassName, pszTitle, dwStyle,
+			prcPosition->left, prcPosition->top, tt::RC_WIDTH(prcPosition), tt::RC_HEIGHT(prcPosition),
+			hwndParent, hmenu, m_hinst, (void*) this);
+	else
+		m_hwnd = ::CreateWindowExA(dwExStyle, m_pszClassName, pszTitle, dwStyle,
+			CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+			hwndParent, hmenu, m_hinst, (void*) this);
+
+	return tt::IsValidWindow(m_hwnd);
+}
+
+bool ttWin::SubClass(HWND hwnd)
+{
+	ttASSERT_MSG(!m_SubClassProc, "You can only subclass a window once!");
+
+	if (m_SubClassProc)
+		return false;
+
+	if (hwnd != NULL)
+		AttachWnd(hwnd);
+
+	SetWindowLongPtrA(hwnd, GWLP_USERDATA, (LONG_PTR) this);
+
+	m_SubClassProc = (WNDPROC) SetWindowLongPtrA(hwnd, GWLP_WNDPROC, (LONG_PTR) ttpriv::ttWinProc);
+
+	return (m_SubClassProc != nullptr);
+}
+
+bool ttWin::AttachWnd(HWND hwnd)
+{
+	if (hwnd == NULL || !IsWindow(hwnd))
+		return false;
+	m_hwnd = hwnd;
+
+	// Deleting m_pwc and getting the window's class name makes it possible to call CreateWnd and create another window
+	// of the same type that we attached to.
+
+	ttStr cszClassName(256);
+	if (::GetClassNameA(hwnd, cszClassName, cszClassName.sizeBuffer()) != 0) {
+		if (m_pwc) {
+			delete m_pwc;
+			m_pwc = nullptr;
+		}
+		SetClassName(cszClassName);	// we store the name separately from m_pwc
+	}
+	m_hwndParent = GetParent(hwnd);
+
+	return true;
+}
