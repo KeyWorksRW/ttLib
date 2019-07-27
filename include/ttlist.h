a8d099fc3efef28e1fc47724bc8e406d0fbe0555
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2019-07-27 13:04:58 -0700

    Comment didn't get updated when flag constants changed

--- include/ttlist.h
+++ include/ttlist.h
@@ -33,7 +33,7 @@ public:
 
     // SetFlags() will not change any previously added strings
 
-    void SetFlags(size_t flags);    // any combination of FLG_PREVENT_DUPLICATES | FLG_IGNORE_CASE | FLG_URL_STRINGS
+    void SetFlags(size_t flags);    // any combination of FLG_ADD_DUPLICATES | FLG_IGNORE_CASE | FLG_URL_STRINGS
     void AllowDuplicates() { SetFlags(FLG_ADD_DUPLICATES); }
 
     size_t  Add(const char* psz);
