c70ae81451dd9f730a959066dd873bea3ac16cd7
KeyWorksRW <42786259+KeyWorksRW@users.noreply.github.com>
2020-01-29 20:58:38 -0800

    Separate Windows code from non-Windows code
    Fixes #115

--- src/ttmisc.cpp
+++ src/ttmisc.cpp
@@ -10,17 +10,6 @@
 
 #include "../include/ttdebug.h"  // for ttASSERTS
 #include "../include/ttstr.h"    // ttCStr
-#include "../include/ttwstr.h"   // ttCWStr
-
-#if defined(_WX_DEFS_H_)
-#include <wx/msgdlg.h>  // wxMessageBox
-#endif
-
-namespace tt
-{
-    const char*    pszMsgTitle;   // utf8  title for message boxes
-    const wchar_t* pwszMsgTitle;  // utf16 title for message boxes
-}  // namespace tt
 
 size_t ttHashFromSz(const char* psz)
 {
@@ -71,55 +60,4 @@ size_t ttHashFromURL(const wchar_t* pszURL)
     ttBackslashToForwardslash(csz);
     csz.MakeLower();
     return ttHashFromSz(csz);
-}
-
-void ttInitCaller(const char* pszTitle)
-{
-#if defined(_WIN32)
-    tt::hinstResources = GetModuleHandle(NULL);
-#endif
-    ttSetMsgBoxTitle(pszTitle);
-}
-
-void ttSetMsgBoxTitle(const char* pszTitle)
-{
-    if (tt::pszMsgTitle)
-        ttFree((void*) tt::pszMsgTitle);
-    tt::pszMsgTitle = ttStrDup(pszTitle ? pszTitle : "");
-
-    if (tt::pwszMsgTitle)
-        ttFree((void*) tt::pwszMsgTitle);
-    if (!pszTitle)
-        tt::pwszMsgTitle = ttStrDup(L"");
-    else
-    {
-        ttCWStr cwsz(pszTitle);
-        tt::pwszMsgTitle = ttStrDup((wchar_t*) cwsz);
-    }
-}
-
-#ifndef _INC_STDLIB
-__declspec(noreturn) void __cdecl exit(int _Code);
-#endif
-
-__declspec(noreturn) void ttOOM(void)
-{
-#if defined(_DEBUG)
-#if defined(_WIN32)
-    int answer = MessageBoxA(GetActiveWindow(), "Out of Memory!!!", "Do you want to call DebugBreak()?",
-                             MB_YESNO | MB_ICONERROR);
-
-    if (answer == IDYES)
-        DebugBreak();
-#else
-    int answer = wxMessageBox("Do you want to call DebugBreak()?", "Out of Memory!!!", wxYES_NO | wxICON_ERROR);
-    if (answer == wxYES)
-        wxTrap();
-#endif
-#endif  // _DEBUG
-
-    // The advantage of exit() is functions registered by atexit() will be called, which might include deleting
-    // temporary files, or other data that might otherwise persist after the program is exited.
-
-    exit(-1);
 }
