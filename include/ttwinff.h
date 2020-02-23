c0959784ebeaf62910d174fdfdc70c5e88b11b58
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2020-02-23 06:32:59 -0800

    Create new version of ttCFindFile
    This uses the WIDE version of the Windows API, providing automatic
    UTF8/UTF16 conversion so that the caller can still work with UTF8 strings.

---
+++ include/ttwinff.h
@@ -0,0 +1,115 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:      tt::winff class
+// Purpose:   Wrapper around Windows FindFile
+// Author:    Ralph Walden
+// Copyright: Copyright (c) 2020 KeyWorks Software (Ralph Walden)
+// License:   Apache License (see ../LICENSE)
+/////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#if !defined(_WIN32)
+    #error "This header file can only be used when compiling for Windows"
+#endif
+
+/// @file
+///
+/// This class uses the WIDE version of the Windows FindFile functionality, converting any name found
+/// into a UTF8 string.
+///
+///    Example usage:
+///
+///    tt::winff ff("*.*");
+///    if (ff.isvalid()) {
+///        do {
+///            if (ff.isdir())
+///                std::cout << "Found the directory " << ff.c_str() << '\n';
+///            else
+///                std::cout << "Found the file " << ff.c_str() << '\n';
+///        } while(ff.next());
+///    }
+
+#include <fileapi.h>
+#include <stdint.h>
+#include <string_view>
+
+#include "ttcstr.h"
+#include "utf8unchecked.h"
+
+#if !defined(INVALID_HANDLE_VALUE)
+    // normally defined in <handleapi.h>
+    #define INVALID_HANDLE_VALUE ((HANDLE)(LONG_PTR) -1)
+#endif
+
+namespace tt
+{
+    class winff : public WIN32_FIND_DATAW
+    {
+    public:
+        winff() { m_hfind = INVALID_HANDLE_VALUE; }  // With this constructor, call NewPattern(...) to initialize
+
+        winff(std::string_view filepattern)
+        {
+            std::wstring str16;
+            utf8::unchecked::utf8to16(filepattern.begin(), filepattern.end(), back_inserter(str16));
+            m_hfind = FindFirstFileExW(str16.c_str(), FindExInfoBasic, this, FindExSearchNameMatch, nullptr,
+                                       FIND_FIRST_EX_LARGE_FETCH);
+            m_filename.from_utf16(cFileName);
+        }
+        ~winff()
+        {
+            if (m_hfind != INVALID_HANDLE_VALUE)
+                FindClose(m_hfind);
+        }
+
+        bool next()
+        {
+            if (FindNextFileW(m_hfind, this))
+            {
+                m_filename.from_utf16(cFileName);
+                return true;
+            }
+            else
+            {
+                m_filename.clear();
+                return false;
+            }
+        }
+
+        bool newpattern(std::string_view filepattern)
+        {
+            if (m_hfind != INVALID_HANDLE_VALUE)
+                FindClose(m_hfind);
+            std::wstring str16;
+            utf8::unchecked::utf8to16(filepattern.begin(), filepattern.end(), back_inserter(str16));
+            m_hfind = FindFirstFileExW(str16.c_str(), FindExInfoBasic, this, FindExSearchNameMatch, nullptr,
+                                       FIND_FIRST_EX_LARGE_FETCH);
+            m_filename.from_utf16(cFileName);
+            return isvalid();
+        }
+
+        const char* c_str() { return m_filename.c_str(); }
+        operator const char*() const { m_filename.c_str(); }
+        operator DWORD() const { return dwFileAttributes; }
+
+        const tt::cstr& getfilename() { return m_filename; }
+
+        bool isarchive() const { return (dwFileAttributes & FILE_ATTRIBUTE_ARCHIVE) ? true : false; }
+        bool iscompressed() const { return (dwFileAttributes & FILE_ATTRIBUTE_COMPRESSED) ? true : false; }
+        bool isdir() const { return (dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) ? true : false; }
+        bool ishidden() const { return (dwFileAttributes & FILE_ATTRIBUTE_HIDDEN) ? true : false; }
+        bool isoffline() const { return (dwFileAttributes & FILE_ATTRIBUTE_OFFLINE) ? true : false; }
+        bool isreadonly() const { return (dwFileAttributes & FILE_ATTRIBUTE_READONLY) ? true : false; }
+        bool issystem() const { return (dwFileAttributes & FILE_ATTRIBUTE_SYSTEM) ? true : false; }
+        bool istemporary() const { return (dwFileAttributes & FILE_ATTRIBUTE_TEMPORARY) ? true : false; }
+        bool isvalid() const { return (m_hfind != INVALID_HANDLE_VALUE) ? true : false; }
+
+        /// Call this before retrieving the filename if you want all backslashes converted to forward slahes.
+        void backslashestoforward() { m_filename.backslashestoforward(); }
+
+    private:
+        HANDLE m_hfind;
+
+        tt::cstr m_filename;
+    };
+}  // namespace tt
