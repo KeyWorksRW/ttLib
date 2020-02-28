c591c9d66f14560debb4e80845374bc2faa69012
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2020-02-28 09:31:22 -0800

    Refactor to use std::string and ttlib:: namespace
    Trying this out as part of the namespace overhaul. Need to see whether
    returning std::string is better or worse for compatibility with other functions.
    The one significant advantage is being able to use:
    
        std::string s = _tt("foo") + " " + _tt("bar");
    
    The above is impossible if you return pointers.

--- include/ttTR.h
+++ include/ttTR.h
@@ -11,40 +11,42 @@
 /// strings will not be translated.
 ///
 /// **_tt()** is similar to the _() macro in **wxWidgets** in that it is used to translate a string.
-/// The most significant difference is that _tt() returns a **const char\*** pointer instead
-/// of a **wxString&**. On Windows, that means you can pass **_tt()** to **std:string** and **std::string_view**
-/// without having to first convert it from a **UTF16** string to a **UTF8** string `wxString.utf8().data()`.
+/// The most significant difference is that _tt() returns **const std::string&** instead of **wxString&**. On
+/// Windows, that means you can pass **_tt()** to **std:string** and **std::string_view** without having to first
+/// convert it from a **UTF16** string to a **UTF8** string `wxString.utf8().data()`.
 ///
 /// **_tt()** will only use the underlying **wxWidgets** library to translate a string once. It then stores
-/// the translation in an **std::unordered_map** and the next time a translation for that string is requested, it
-/// will return a pointer to the already-translated string.
+/// the translation in an **std::map** and the next time a translation for that string is requested, it
+/// will return the already-translated string.
 ///
-/// If you need to change languages within the program, you must call **ttTRclear()** to clear any previously
-/// stored translated strings.
+/// If you need to change languages within the program, you must call **ttlib::clearTranslations()** to clear any
+/// previously stored translated strings.
 ///
 /// Note: If you use these functions/macro, be sure to add `-k_tt -k_ttp:1,2 -kttTR` to your **xgettext**
 /// command line.
 
 #pragma once
 
-/// Macro that can be parsed by xgettext to add a string to translate.
-#define _tt(txt) ttTranslate(txt)
+#include <string>
 
-/// Looks up the translation of the string, returns a pointer to the translation, or a copy of the original
-/// string if no translation is available.
-const char* ttTranslate(const char* psz);
-
-/// If n == 1, returns a translation of psz, otherwise it returns a translation of pszPlural.
-inline const char* _ttp(const char* psz, const char* pszPlural, unsigned int n)
+namespace ttlib
 {
-    return (n == 1 ? _tt(psz) : _tt(pszPlural));
-}
+    /// Returns either a translated string, or the original string if no translation is available.
+    const std::string& translate(const std::string& str);
 
-/// Clears all previously translated strings. Required after changing locale.
-void ttTRclear() noexcept;
+    /// Clears all previously translated strings. Required after changing locale.
+    void clearTranslations() noexcept;
+}  // namespace ttlib
 
-/// Shorter version of wxTRANSLATE() macro which does the same thing.
-///
+/// Macro that can be parsed by xgettext to add a string to translate.
+#define _tt(txt) ttlib::translate(txt)
+
 /// This macro can be placed around static text that you want xgettext.exe to extract for
-/// translation using the "xgettext.exe -kttTTRS" keyword option.
+/// translation using the "xgettext.exe -kttTR" keyword option.
 #define ttTR(txt) txt
+
+/// If count == 1, returns a translation of single, otherwise it returns a translation of plural.
+inline const std::string& _ttp(const std::string& single, const std::string& plural, unsigned int count)
+{
+    return (count == 1 ? _tt(single) : _tt(plural));
+}
--- src/ttTR.cpp
+++ src/ttTR.cpp
@@ -6,11 +6,11 @@
 // License:   Apache License (see LICENSE)
 /////////////////////////////////////////////////////////////////////////////
 
-// Currently, translation only occurs when use with wxWidgets.
+// Note: currently, translation only occurs when compiled with wxWidgets.
 
 #include "pch.h"
 
-#include <unordered_map>
+#include <map>
 
 #if defined(_WX_DEFS_H_)
     #include <wx/intl.h>
@@ -18,7 +18,65 @@
 
 #include "../include/ttTR.h"
 
-std::unordered_map<std::string, std::string> tt_translations;
+std::map<const std::string, std::string> tt_translations;
+
+const std::string& ttlib::translate(const std::string& str)
+{
+    if (str.empty())
+        return str;
+
+    auto found = tt_translations.find(str);
+    if (found != tt_translations.end())
+    {
+        return found->second;
+    }
+    else
+    {
+#if defined(_WX_DEFS_H_)
+        auto trans = wxTranslations::Get();
+        if (trans)
+        {
+            auto strTranslation = trans->GetTranslatedString(psz);
+            if (strTranslation)
+            {
+                auto translated = tt_translations.insert({ psz, strTranslation->utf8_str().data() });
+                if (translated.second)
+                {
+                    return translated.first->second.c_str();
+                }
+            }
+            else
+            {
+                // No translation available, make certain we don't try again.
+                auto translated = tt_translations.insert({ psz, psz });
+                if (translated.second)
+                {
+                    return translated.first->second.c_str();
+                }
+            }
+        }
+        else
+        {
+            auto translated = tt_translations.insert({ psz, psz });
+            if (translated.second)
+            {
+                return translated.first->second.c_str();
+            }
+        }
+#else   // !defined(_WX_DEFS_H_)
+        // Currently all that happens is the string is added without translation.
+        auto translated = tt_translations.insert({ str, str });
+        if (translated.second)
+        {
+            return translated.first->second;
+        }
+#endif  // _WX_DEFS_H_
+    }
+
+    // We only get here if the passed string ptr is null, or the unordered_map was unable to insert
+    // the string.
+    return str;
+}
 
 const char* ttTranslate(const char* psz)
 {
@@ -62,8 +120,8 @@ const char* ttTranslate(const char* psz)
                     return translated.first->second.c_str();
                 }
             }
-#else    // !defined(_WX_DEFS_H_)
-            // Currently all that happens is the string is added without translation.
+#else   // !defined(_WX_DEFS_H_)
+        // Currently all that happens is the string is added without translation.
             auto translated = tt_translations.insert({ psz, psz });
             if (translated.second)
             {
@@ -78,7 +136,7 @@ const char* ttTranslate(const char* psz)
     return psz;
 }
 
-void ttTRclear() noexcept
+void clearTranslations() noexcept
 {
     tt_translations.clear();
 }
