f40dc7a25c24baba20ad1e3c75e9897c576dd0d9
KeyWorksRW <42786259+KeyWorksRW@users.noreply.github.com>
2020-04-03 09:22:54 -0700

    Implement new ttlib::ThrdPool class
    Fixes #145

---
+++ include/ttthrdpool.h
@@ -0,0 +1,88 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:      ThrdPool, CMultiChildThread
+// Purpose:   Class for handling multiple threads
+// Author:    Ralph Walden
+// Copyright: Copyright (c) 2010-2020 KeyWorks Software (Ralph Walden)
+// License:   Apache License (see ../LICENSE)
+/////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+// REVIEW: [KeyWorks - 04-01-2020] Once C++20 becomes available, this will be revisited to see if can be
+// implemented without requiring Windows. The current issue is the call to WaitForMultipleObjects -- this allows
+// for a single call to wait for multiple threads to complete, and returns as soon as one of them is available.
+// Without this call, the program must use a polling loop, tying up a CPU looping over and over waiting for one of
+// the threads to finish.
+
+#if !defined(_WIN32)
+    #error "This header file can only be used when compiling for Windows"
+#endif
+
+// This class creates a pool of threads. The threads will exist until this class's destructor is called. The class
+// is designed to handle a scenario where you have a single function that you want to run dozens or even thousands
+// of times, such as a grep function that is looking for a specific string in thousands of files. By using a pool
+// of available threads, it avoids the overhead of thread creation/destruction, as well as ensuring that one
+// thread is always available for UI or other processing needed by the caller.
+
+// To use a thread, call StartThread(pData) -- the thread will then call the function you passed to the ThrdPool
+// constructor passing it the pData argument. What you point to with pData is entirely up to you -- ThrdPool
+// will do nothing with this argument other than pass it to your function.
+
+// Call WaitForThreadsToComplete() to wait for all threads to finish normally.
+
+#include <wtypes.h>
+
+#include <functional>
+#include <vector>
+
+using ThreadFunction = std::function<void(void* pdata)>;
+
+namespace ttlib
+{
+    DWORD WINAPI PoolThread(void* pv);
+
+    /// Class for handling a pool of multiple threads
+    class ThrdPool
+    {
+    public:
+        /// Construct with a pointer to the function each thread should call
+        ThrdPool(ThreadFunction function);
+        ~ThrdPool();
+
+        /// This will not return until an available thread from the pool is found
+        void StartThread(void* pData);
+
+        /// This will not return until all started threads have returned from their call to your ThreadFunction
+        void WaitForThreadsToComplete();
+
+    protected:
+        struct THRD_INFO
+        {
+            void* pData;  // data passed to the worker thread
+
+            HANDLE hsemStart;
+            HANDLE hsemDone;
+            HANDLE hThread;
+            DWORD thrdID;
+        };
+
+        // This is the function called by CreateThread. It handles all the signalling as well as calling the
+        // caller-supplied function.
+        friend DWORD WINAPI PoolThread(void* pv);
+
+    private:
+        std::vector<THRD_INFO> m_threads;
+
+        // Number of threads created (CPUs - 1)
+        DWORD m_cThreads;
+
+        // This is the caller-supplied function that each thread will call
+        ThreadFunction m_function;
+
+        // This is used in calls to WaitForMultipleObjects
+        HANDLE* m_ahsemDone { nullptr };
+
+        // This is set to true in the destructor and is used to terminate all threads
+        bool m_isEndAllThreads { false };
+    };
+}  // namespace ttlib
---
+++ src/winsrc/ttthrdpool.cpp
@@ -0,0 +1,123 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:      ttlib::ThrdPool
+// Purpose:   Class for utilizing Windows heap manager
+// Author:    Ralph Walden
+// Copyright: Copyright (c) 2010-2020 KeyWorks Software (Ralph Walden)
+// License:   Apache License (see ../LICENSE)
+/////////////////////////////////////////////////////////////////////////////
+
+#include "pch.h"
+
+#if !defined(_WIN32)
+    #error "This module can only be compiled for Windows"
+#endif
+
+#include <cassert>
+#include <exception>
+
+#include "ttdebug.h"
+#include "ttthrdpool.h"
+
+using namespace ttlib;
+
+ThrdPool::ThrdPool(ThreadFunction function)
+{
+    m_function = function;
+
+    SYSTEM_INFO si;
+    GetSystemInfo(&si);
+
+    // Always leave one CPU available. It will remain available to the caller for handling UI, I/O, etc.
+    m_cThreads = si.dwNumberOfProcessors - 1;
+    m_threads.resize(m_cThreads);
+    m_ahsemDone = static_cast<HANDLE*>(std::malloc(sizeof(HANDLE) * m_cThreads));
+    if (!m_ahsemDone)
+        throw std::bad_alloc();
+
+    DWORD thrd;
+    for (thrd = 0; thrd < m_cThreads; ++thrd)
+    {
+        auto& ThreadInfo = m_threads[thrd];
+        ThreadInfo.hsemStart = CreateSemaphore(NULL, 0, 1, NULL);
+        ThreadInfo.hsemDone = CreateSemaphore(NULL, 1, 1, NULL);
+        m_ahsemDone[thrd] = ThreadInfo.hsemDone;
+
+        ThreadInfo.hThread = CreateThread(NULL, 0, PoolThread, static_cast<LPVOID>(this), 0, &ThreadInfo.thrdID);
+        assert(ThreadInfo.hThread);
+        if (!ThreadInfo.hThread)
+        {
+            --m_cThreads;
+            continue;
+        }
+    }
+
+};
+
+ThrdPool::~ThrdPool()
+{
+    m_isEndAllThreads = true;
+
+    for (auto& ThrdInfo: m_threads)
+    {
+        ReleaseSemaphore(ThrdInfo.hsemStart, 1, NULL);    // thread can't exit until you let it start
+        WaitForSingleObject(ThrdInfo.hThread, INFINITE);  // wait for thread to terminate
+        CloseHandle(ThrdInfo.hsemStart);
+        CloseHandle(ThrdInfo.hsemDone);
+        CloseHandle(ThrdInfo.hThread);
+    }
+
+    if (m_ahsemDone)
+        std::free(m_ahsemDone);
+}
+
+void ThrdPool::StartThread(void* pData)
+{
+    DWORD pos = WaitForMultipleObjects(m_cThreads, m_ahsemDone, FALSE, INFINITE) - WAIT_OBJECT_0;
+    assert(pos >= 0 && pos < m_cThreads);
+    m_threads[pos].pData = pData;
+    ReleaseSemaphore(m_threads[pos].hsemStart, 1, NULL);
+}
+
+void ThrdPool::WaitForThreadsToComplete()
+{
+    // Wait until all threads are done.
+    WaitForMultipleObjects(m_cThreads, m_ahsemDone, TRUE, INFINITE);
+
+    // All Done semaphores are now non-signalled, so we need to signal them all so that we can wait on them again
+    for (size_t thrd = 0; thrd < m_cThreads; thrd++)
+        ReleaseSemaphore(m_ahsemDone[thrd], 1, NULL);
+}
+
+DWORD WINAPI ttlib::PoolThread(void* pv)
+{
+    auto ThrdPool  = reinterpret_cast<ttlib::ThrdPool*>(pv);
+    DWORD thrdID = GetCurrentThreadId();
+    for (auto& thrdInfo: ThrdPool->m_threads)
+    {
+        if (thrdInfo.thrdID == thrdID)
+        {
+            for (;;)
+            {
+                if (WaitForSingleObject(thrdInfo.hsemStart, INFINITE) != WAIT_OBJECT_0)
+                    break;
+                if (ThrdPool->m_isEndAllThreads)
+                    return 0;
+
+                try
+                {
+                    ThrdPool->m_function(thrdInfo.pData);
+                }
+                catch (...)
+                {
+                    // This catch is a last-ditch effort to keep the program from crashing. The caller should
+                    // have handled exceptions in m_function().
+                    assert(false);
+                }
+
+                ReleaseSemaphore(thrdInfo.hsemDone, 1, NULL);
+            }
+            break;
+        }
+    }
+    return 0;
+}
